4c13d37bd3f7d2894c72bc42a6007eca93470bf7
Phillip Hwang <thehunter9709@gmail.com>
2018-06-01 08:13:29 -0400

    Done fucking around with Animation

---
+++ Diamond Hunter/src/Animation.java
@@ -0,0 +1,85 @@
+// This class takes in an array of images.
+// Calling getImage() gives you the appropriate
+// image in the Animate cycle.
+
+
+
+import java.awt.image.BufferedImage;
+
+public class Animation {
+
+	private BufferedImage[] squareTiles;
+	private int currentsquareTile;
+	private int numsquareTiles;
+
+	private int numCount;
+	private int timeDelay;
+
+	private int timesRun;
+
+	
+	public Animation() {
+		timesRun = 0;
+	}
+	
+
+	public boolean hasPlayedOnce() { 
+		return timesRun > 0;
+	}
+	public boolean hasPlayed(int i) { 
+		return timesRun == i;
+	}
+	
+	public void update() {
+
+		if(timeDelay == -1) return;
+
+		numCount++;
+
+		if(numCount == timeDelay) {
+			currentsquareTile++;
+			numCount = 0;
+		}
+		if(currentsquareTile == numsquareTiles) {
+			currentsquareTile = 0;
+			timesRun++;
+		}
+
+	}
+	
+	
+
+	public void setsquareTile(int i) { 
+		currentsquareTile = i; 
+	}
+	
+
+	public int getsquareTile() { 
+		return currentsquareTile;
+	}
+	public void setNumsquareTiles(int i) { 
+		numsquareTiles = i;
+	}
+
+	
+	public void setsquareTiles(BufferedImage[] squareTiles) {
+		this.squareTiles = squareTiles;
+		currentsquareTile = 0;
+		numCount = 0;
+		timesRun = 0;
+		timeDelay = 2;
+		numsquareTiles = squareTiles.length;
+	}
+
+	public void settimeDelay(int i) { 
+		timeDelay = i;
+	}
+
+	public int getnumCount() { 
+		return numCount;
+	}
+	public BufferedImage getImage() { 
+		return squareTiles[currentsquareTile];
+	}
+
+}
---
+++ Diamond Hunter/src/Data.java
@@ -0,0 +1,11 @@
+// Global helper class.
+// Records finish time upon game completion.
+
+public class Data {
+	
+	public static long time;
+	
+	public static void setTime(long l) { time = l; }
+	public static long getTime() { return time; }
+	
+}
