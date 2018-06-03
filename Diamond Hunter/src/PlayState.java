1c7bee233e671e40e8d3ea3c8cf9076881ea7cfa
Phillip Hwang <thehunter9709@gmail.com>
2018-06-03 12:59:42 -0400

    Removed items and all sound

--- Diamond Hunter/src/PlayState.java
+++ Diamond Hunter/src/PlayState.java
@@ -4,82 +4,78 @@ import java.awt.Rectangle;
 import java.util.ArrayList;
 
 public class PlayState extends GameState {
-	
+
 	// player
 	private Player player;
-	
+
 	// tilemap
 	private TileMap tileMap;
-	
+
 	// bones
 	private ArrayList<Bone> bones;
-	
-	// items
-	private ArrayList<Item> items;
-	
+
+
 	private int eventTick;
 	// camera position
 	private int xsector;
 	private int ysector;
 	private int sectorSize; 
-	
+
 	// hud
 	private Hud hud;
-	
+
 	// events
 	private boolean blockInput;
 	private boolean eventStart;
 	private boolean eventFinish;
-	
+
 	// transition box
 	private ArrayList<Rectangle> boxes;
-	
+
 	public PlayState(GameStateManager gsm) {
 		super(gsm);
 	}
-	
+
 	public void init() {
-		
+
 		// create lists
 		bones = new ArrayList<Bone>();
-		items = new ArrayList<Item>();
-		
+
 		// load map
 		tileMap = new TileMap(16);
 		tileMap.loadTiles("/Tilesets/testtileset.gif");
 		tileMap.loadMap("/Maps/testmap.map");
-		
+
 		// create player
 		player = new Player(tileMap);
-		
+
 		// fill lists
 		populateBones();
-		populateItems();
-		
+
 		// initialize player
 		player.setTilePosition(17, 17);
 		player.setTotalBones(bones.size());
-		
+
 		// set up camera position
 		sectorSize = GamePanel.WIDTH;
 		xsector = player.getx() / sectorSize;
 		ysector = player.gety() / sectorSize;
 		tileMap.setPositionImmediately(-xsector * sectorSize, -ysector * sectorSize);
-		
+
 		// load hud
 		hud = new Hud(player, bones);
-		
+
 		// start event
 		boxes = new ArrayList<Rectangle>();
 		eventStart = true;
 		eventStart();
-			
+
 	}
-	
+
 	private void populateBones() {
-		
+
 		Bone d;
-		
+
 		d = new Bone(tileMap);
 		d.setTilePosition(20, 20);
 		d.addChange(new int[] { 23, 19, 1 });
@@ -98,7 +94,7 @@ public class PlayState extends GameState {
 		d.setTilePosition(4, 34);
 		d.addChange(new int[] { 31, 21, 1 });
 		bones.add(d);
-		
+
 		d = new Bone(tileMap);
 		d.setTilePosition(28, 19);
 		bones.add(d);
@@ -132,38 +128,23 @@ public class PlayState extends GameState {
 		d = new Bone(tileMap);
 		d.setTilePosition(13, 20);
 		bones.add(d);
-		
+
 	}
-	
-	private void populateItems() {
-		
-		Item item;
-		
-		item = new Item(tileMap);
-		item.setType(Item.AXE);
-		item.setTilePosition(26, 37);
-		items.add(item);
-		
-		item = new Item(tileMap);
-		item.setType(Item.BOAT);
-		item.setTilePosition(12, 4);
-		items.add(item);
-		
-	}
-	
+
+
 	public void update() {
-		
+
 		// check keys
 		handleInput();
-		
+
 		// check events
 		if(eventStart) eventStart();
 		if(eventFinish) eventFinish();
-		
+
 		if(player.numBones() == player.getTotalBones()) {
 			eventFinish = blockInput = true;
 		}
-		
+
 		// update camera
 		int oldxs = xsector;
 		int oldys = ysector;
@@ -171,79 +152,63 @@ public class PlayState extends GameState {
 		ysector = player.gety() / sectorSize;
 		tileMap.setPosition(-xsector * sectorSize, -ysector * sectorSize);
 		tileMap.update();
-		
+
 		if(tileMap.isMoving()) return;
-		
+
 		// update player
 		player.update();
-		
+
 		// update bones
 		for(int i = 0; i < bones.size(); i++) {
-			
+
 			Bone d = bones.get(i);
 			d.update();
-			
+
 			// player collects diamond
 			if(player.intersects(d)) {
-				
+
 				// remove from list
 				bones.remove(i);
 				i--;
-				
+
 				// increment amount of collected bones
 				player.collectedBone();
-				
-				
+
+
 				// make any changes to tile map
 				ArrayList<int[]> ali = d.getChanges();
 				for(int[] j : ali) {
 					tileMap.setTile(j[0], j[1], j[2]);
 				}
-				
 			}
 		}
-		// update items
-		for(int i = 0; i < items.size(); i++) {
-			Item item = items.get(i);
-			if(player.intersects(item)) {
-				items.remove(i);
-				i--;
-				item.collected(player);
-			}
-		}
-		
 	}
-	
+
 	public void draw(Graphics2D g) {
-		
+
 		// draw tilemap
 		tileMap.draw(g);
-		
+
 		// draw player
 		player.draw(g);
-		
+
 		// draw bones
 		for(Bone d : bones) {
 			d.draw(g);
 		}
-		
-		
-		// draw items
-		for(Item i : items) {
-			i.draw(g);
-		}
-		
+
+
 		// draw hud
 		hud.draw(g);
-		
+
 		// draw transition boxes
 		g.setColor(java.awt.Color.BLACK);
 		for(int i = 0; i < boxes.size(); i++) {
 			g.fill(boxes.get(i));
 		}
-		
+
 	}
-	
+
 	public void handleInput() {
 		if(Keys.isPressed(Keys.ESCAPE)) {
 			gsm.setPaused(true);
@@ -255,9 +220,9 @@ public class PlayState extends GameState {
 		if(Keys.isDown(Keys.DOWN)) player.setDown();
 		if(Keys.isPressed(Keys.SPACE)) player.setAction();
 	}
-	
+
 	//===============================================
-	
+
 	private void eventStart() {
 		eventTick++;
 		if(eventTick == 1) {
@@ -283,7 +248,7 @@ public class PlayState extends GameState {
 			eventTick = 0;
 		}
 	}
-	
+
 	private void eventFinish() {
 		eventTick++;
 		if(eventTick == 1) {
@@ -305,5 +270,5 @@ public class PlayState extends GameState {
 			}
 		}
 	}
-	
+
 }
